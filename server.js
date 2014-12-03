cfbef9aca9970fe4540921b03eade603520deed7
Felix.G <felixgrayson@hotmail.com>
2014-12-03 23:12:23 +1300

    Update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var express = require('express'),
 
 var routes = require('./router/index');
 
+
 var app = express();
 
 //view engine setup
@@ -19,11 +20,13 @@ app.set('view engine','ejs');
 
 app.use(logger('dev'));
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({extended:false}));
 app.use(cookieparser());
 app.use(express.static(path.join(__dirname + "/views")));
+app.use('/public', express.static(path.join(__dirname + "/public")));
 app.use('/components', express.static(path.join(__dirname + "/components")));
 app.use('/controllers', express.static(path.join(__dirname + "/controllers")));
+app.use('/filters', express.static(path.join(__dirname + "/filters")));
 app.use('/', routes);
 
 app.use(function(req,res,next){
@@ -32,9 +35,15 @@ app.use(function(req,res,next){
     next(err);
 });
 
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
-app.set('port', process.env.PORT || 3000);
-
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'),function(){
     debug('Express server listening on port'+server.address().port);
